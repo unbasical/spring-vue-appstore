4945c375b036db17155ab6215b3f0a696de2f30c
Toni <a.saatze@googlemail.com>
2019-04-26 09:49:13 +0200

    POJO for Comment (a comment does not need to know on which App it refers to, the App does know which comments are refering to itself)

--- src/main/java/base/appstore/model/Comment.java
+++ src/main/java/base/appstore/model/Comment.java
@@ -1,5 +1,35 @@
 package base.appstore.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Comment {
+
+    @Id
+    @Column(name = "comment_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String commentText;
+
+    @CreatedDate
+    private Date createDate;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "comment_id")
+    private User author;
 
 }

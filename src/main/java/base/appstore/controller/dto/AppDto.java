2be25e02812ce4be4af09765e3bed066d9c23703
Daniel Seifert <seifert-daniel1@gmx.de>
2019-05-09 14:17:07 +0200

    Added screenshots to appDTO

--- src/main/java/base/appstore/controller/dto/AppDto.java
+++ src/main/java/base/appstore/controller/dto/AppDto.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,6 +16,7 @@ public class AppDto {
     private Long id;
     private String title;
     private String description;
+    private List<ScreenshotDto> screenshots;
     private long views;
 
     public AppDto(App app) {
@@ -20,6 +24,7 @@ public class AppDto {
         this.title = app.getTitle();
         this.description = app.getDescription();
         this.views = app.getViews();
+        this.screenshots = app.getScreenshots().stream().map(ScreenshotDto::new).collect(Collectors.toList());
     }
 
     public App toEntity() {

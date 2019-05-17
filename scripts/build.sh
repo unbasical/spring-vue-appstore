550d61bfedbb337d7bdab407c3cba9fe243f5835
Daniel Seifert <seifert-daniel1@gmx.de>
2019-05-17 09:59:26 +0200

    Fixed scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,4 @@
 cd ./src/main/resources/hm-apps && npm run build && rm -r ./../www/static/*
-mkdir ./../www/static && cp -r ./dist/* ./../www/static
+mkdir ./../www/static
+cp -r ./dist/* ./../www/static
 
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 cd ./src/main/resources/hm-apps && npm run build && rm -r ./../www/static/*
-mkdir ./../www/static && cp -r ./dist/* ./../www/static
+mkdir ./../www/static
+cp -r ./dist/* ./../www/static
 cd ./../../../../ && git add . && git commit -m "Changes for deployment" && git push heroku master
 

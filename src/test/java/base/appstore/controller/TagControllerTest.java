f6e328a0047c116be0259c6c88b9d9e614b02367
Daniel Seifert <seifert-daniel1@gmx.de>
2019-05-19 20:53:59 +0200

    Added more tests

---
+++ src/test/java/base/appstore/controller/TagControllerTest.java
@@ -0,0 +1,47 @@
+package base.appstore.controller;
+
+import base.appstore.model.Tag;
+import base.appstore.repository.TagRepository;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TagControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TagRepository tagRepo;
+
+    @Before
+    public void initData() {
+        tagRepo.save(new Tag(null, "Test"));
+    }
+
+    @After
+    public void cleanData() {
+        tagRepo.deleteAll();
+    }
+
+    @Test
+    public void getAllTags() throws Exception {
+        //testing thymeleaf sec:authorize with hasPermission
+        this.mockMvc.perform(get("/api/tags").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+}

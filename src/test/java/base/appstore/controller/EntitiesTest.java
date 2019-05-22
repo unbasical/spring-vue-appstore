89305b0e7e4d3ca53b44552ce39e000c3e04ddd6
Daniel Seifert <seifert-daniel1@gmx.de>
2019-05-22 17:41:16 +0200

    Fixed smells

--- src/test/java/base/appstore/controller/EntitiesTest.java
+++ src/test/java/base/appstore/controller/EntitiesTest.java
@@ -1,7 +1,6 @@
 package base.appstore.controller;
 
-import base.appstore.model.App;
-import base.appstore.model.Comment;
+import base.appstore.model.*;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -10,29 +9,94 @@ import static org.junit.Assert.assertNotEquals;
 public class EntitiesTest {
 
     @Test
-    public void getTestApp() throws Exception {
-        App app1 = App.builder().title("A").description("A").build();
-        App app2 = App.builder().title("B").description("B").build();
-        App app3 = App.builder().title("A").description("A").build();
+    public void testApp() throws Exception {
+        App a1 = App.builder().title("A").description("A").build();
+        App a2 = App.builder().title("B").description("B").build();
+        App a3 = App.builder().title("A").description("A").build();
 
-        assertEquals(app1, app1);
-        assertEquals(app1, app3);
-        assertNotEquals(app1, app2);
-        assertEquals(app1.hashCode(), app3.hashCode());
-        assertNotEquals(app1.hashCode(), app2.hashCode());
+        assertEquals(a1, a1);
+        assertEquals(a1, a3);
+        assertNotEquals(a1, a2);
+        assertEquals(a1.hashCode(), a3.hashCode());
+        assertNotEquals(a1.hashCode(), a2.hashCode());
     }
 
     @Test
-    public void getTestComment() throws Exception {
-        Comment c1 = new Comment(1L, "A", null, null);
-        Comment c2 = new Comment(2L, "B", null, null);
-        Comment c3 = new Comment(1L, "A", null, null);
+    public void testComment() throws Exception {
+        Comment a1 = new Comment(1L, "A", null, null);
+        Comment a2 = new Comment(2L, "B", null, null);
+        Comment a3 = new Comment(1L, "A", null, null);
 
-        assertEquals(c1, c1);
-        assertEquals(c1, c3);
-        assertNotEquals(c1, c2);
-        assertEquals(c1.hashCode(), c3.hashCode());
-        assertNotEquals(c1.hashCode(), c2.hashCode());
+        assertEquals(a1, a1);
+        assertEquals(a1, a3);
+        assertNotEquals(a1, a2);
+        assertEquals(a1.hashCode(), a3.hashCode());
+        assertNotEquals(a1.hashCode(), a2.hashCode());
+    }
+
+    @Test
+    public void testLogo() throws Exception {
+        Logo a1 = Logo.builder().id(1L).filename("Test").contentType("application/json").build();
+        Logo a2 = Logo.builder().id(2L).filename("Test1").contentType("application/json").build();
+        Logo a3 = Logo.builder().id(1L).filename("Test").contentType("application/json").build();
+
+        assertEquals(a1, a1);
+        assertEquals(a1, a3);
+        assertNotEquals(a1, a2);
+        assertEquals(a1.hashCode(), a3.hashCode());
+        assertNotEquals(a1.hashCode(), a2.hashCode());
+    }
+
+    @Test
+    public void testRating() throws Exception {
+        Rating a1 = new Rating(1L, 5, null, null);
+        Rating a2 = new Rating(2L, 2, null, null);
+        Rating a3 = new Rating(1L, 5, null, null);
+
+        assertEquals(a1, a1);
+        assertEquals(a1, a3);
+        assertNotEquals(a1, a2);
+        assertEquals(a1.hashCode(), a3.hashCode());
+        assertNotEquals(a1.hashCode(), a2.hashCode());
+    }
+
+    @Test
+    public void testScreenshot() throws Exception {
+        Screenshot a1 = Screenshot.builder().id(1L).filename("Test").contentType("application/json").build();
+        Screenshot a2 = Screenshot.builder().id(2L).filename("Test1").contentType("application/json").build();
+        Screenshot a3 = Screenshot.builder().id(1L).filename("Test").contentType("application/json").build();
+
+        assertEquals(a1, a1);
+        assertEquals(a1, a3);
+        assertNotEquals(a1, a2);
+        assertEquals(a1.hashCode(), a3.hashCode());
+        assertNotEquals(a1.hashCode(), a2.hashCode());
+    }
+
+    @Test
+    public void testTag() throws Exception {
+        Tag a1 = new Tag(1L, "Test");
+        Tag a2 = new Tag(2L, "Test1");
+        Tag a3 = new Tag(1L, "Test");
+
+        assertEquals(a1, a1);
+        assertEquals(a1, a3);
+        assertNotEquals(a1, a2);
+        assertEquals(a1.hashCode(), a3.hashCode());
+        assertNotEquals(a1.hashCode(), a2.hashCode());
+    }
+
+    @Test
+    public void testUser() throws Exception {
+        User a1 = User.builder().name("UserA").email("a@test.de").build();
+        User a2 = User.builder().name("UserB").email("b@test.de").build();
+        User a3 = User.builder().name("UserA").email("a@test.de").build();
+
+        assertEquals(a1, a1);
+        assertEquals(a1, a3);
+        assertNotEquals(a1, a2);
+        assertEquals(a1.hashCode(), a3.hashCode());
+        assertNotEquals(a1.hashCode(), a2.hashCode());
     }
 
 }

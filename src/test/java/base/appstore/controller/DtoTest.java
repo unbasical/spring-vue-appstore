00edd0fc991b5363ad4f592c34af136b173985d8
Daniel Seifert <seifert-daniel1@gmx.de>
2019-05-23 15:16:21 +0200

    Added more tests

--- src/test/java/base/appstore/controller/DtoTest.java
+++ src/test/java/base/appstore/controller/DtoTest.java
@@ -1,28 +1,61 @@
 package base.appstore.controller;
 
-import base.appstore.controller.dto.AppDto;
+import base.appstore.controller.dto.*;
+import com.openpojo.reflection.PojoClass;
+import com.openpojo.reflection.impl.PojoClassFactory;
+import com.openpojo.validation.Validator;
+import com.openpojo.validation.ValidatorBuilder;
+import com.openpojo.validation.rule.impl.GetterMustExistRule;
+import com.openpojo.validation.rule.impl.SetterMustExistRule;
+import com.openpojo.validation.test.impl.GetterTester;
+import com.openpojo.validation.test.impl.SetterTester;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
-import static org.junit.Assert.assertEquals;
-
+@RunWith(Parameterized.class)
 public class DtoTest {
 
-    @Test
-    public void testApp() throws Exception {
-        AppDto dto = new AppDto();
-        dto.setId(1L);
-        dto.setTitle("Test");
-        dto.setRating(5);
-        dto.setDescription("Test");
-        dto.setScreenshots(new ArrayList<>());
-        dto.setTags(new ArrayList<>());
-        dto.setViews(10);
+    private final Class classToTest;
 
-        assertEquals(1L, dto.getId().longValue());
-        assertEquals("Test", dto.getTitle());
-        assertEquals(5, dto.getRating(), 1);
+    public DtoTest(Class classToTest) {
+        this.classToTest = classToTest;
     }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {AppDto.class},
+                {AuthenticationRequest.class},
+                {CommentDto.class},
+                {JWTTokenResponse.class},
+                {RatingDto.class},
+                {ScreenshotDto.class},
+                {TagDto.class},
+                {UploadFileResponse.class},
+                {UserDto.class}
+        });
+    }
+
+
+
+    @Test
+    public void validateSettersAndGetters() {
+        PojoClass activityPojo = PojoClassFactory.getPojoClass(classToTest);
+
+        Validator validator = ValidatorBuilder.create()
+                // Lets make sure that we have a getter and a setter for every field defined.
+                .with(new SetterMustExistRule()).with(new GetterMustExistRule())
+
+                // Lets also validate that they are behaving as expected
+                .with(new SetterTester()).with(new GetterTester()).build();
+
+        // Start the Test
+        validator.validate(activityPojo);
+    }
+
 
 }

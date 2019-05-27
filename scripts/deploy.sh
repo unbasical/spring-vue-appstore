cedada6bf555ed0a2bcbfd7556268ec71c0268ce
Daniel Seifert <seifert-daniel1@gmx.de>
2019-05-27 21:51:39 +0200

    Changes for deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-cd ./src/main/resources/hm-apps && npm run build && rm -r ./../www/static/*
+cd ./src/main/resources/hm-apps && npm run build && rm -rf ./../www/static
 mkdir ./../www/static
 cp -r ./dist/* ./../www/static
 cd ./../../../../ && git add . && git commit -m "Changes for deployment"

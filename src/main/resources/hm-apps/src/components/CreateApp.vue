afd0bc9d11cd1af7173f89355aa7267fe6dbb4bc
Daniel Seifert <seifert-daniel1@gmx.de>
2019-06-12 11:01:38 +0200

    Enhanced app creation process

--- src/main/resources/hm-apps/src/components/CreateApp.vue
+++ src/main/resources/hm-apps/src/components/CreateApp.vue
@@ -1,46 +1,112 @@
 <template>
-    <form>
-        <v-card style="margin-left: 25%;margin-right: 25%;margin-top: 15px">
-            <v-text-field
-                    v-model="title"
-                    label="Title:"
-                    required
-                    style="margin: 3%;"
-            ></v-text-field>
-            <v-textarea
-                    v-model="description"
-                    label="App description"
-                    required
-                    style="margin: 3%;"
-            >
-            </v-textarea>
-            <v-select
-                    :items="allTags"
-                    v-model="tags"
-                    chips
-                    multiple
-                    label="Tags"
-                    style="margin: 3%;"
-            ></v-select>
-            <v-layout justify-space-between row style="margin: 3%;">
-                Screenshots:
-                <input multiple type="file" @change="onScreenshotFilesSelected">
-                <v-layout row v-for="pic in screenshotFiles">
-                    <v-img :src="getScreenshotUrl(pic)" v-if="pic!=null" contain height="50px"></v-img>
-                </v-layout>
-            </v-layout>
-            <v-layout justify-space-between row style="margin: 3%;">
-                Logo:
-                <input type="file" @change="onFileSelected">
-                <v-img :src="getScreenshotUrl(logo)" v-if="logo!=null" contain height="50px"></v-img>
-            </v-layout>
-            <v-btn
-                    @click="createApp"
-                    style="margin: 3%;"
-            > Create My APP
-            </v-btn>
-        </v-card>
-    </form>
+    <v-stepper v-model="stage" style="margin-left: 25%;margin-right: 25%;margin-top: 15px">
+        <v-stepper-header>
+            <v-stepper-step :complete="stage > 1" step="1">App Beschreiben</v-stepper-step>
+
+            <v-divider></v-divider>
+
+            <v-stepper-step :complete="stage > 2" step="2">Tags hinzufügen</v-stepper-step>
+
+            <v-divider></v-divider>
+
+            <v-stepper-step step="3">Bilder hochladen</v-stepper-step>
+        </v-stepper-header>
+
+        <v-stepper-items>
+            <v-stepper-content step="1">
+                <v-card
+                        class="mb-5"
+                        color="grey lighten-3"
+                        height="500px"
+                        style="padding: 30px;"
+                >
+                    <form>
+                        <v-text-field
+                                v-model="title"
+                                label="Titel:"
+                                required
+                                style="margin: 3%;"
+                        ></v-text-field>
+                        <v-textarea
+                                v-model="description"
+                                label="Beschreibung"
+                                required
+                                style="margin: 3%;"
+                        >
+                        </v-textarea>
+                    </form>
+                </v-card>
+
+                <v-btn
+                        color="primary"
+                        @click="createApp"
+                >
+                    Weiter
+                </v-btn>
+            </v-stepper-content>
+
+            <v-stepper-content step="2">
+                <v-card
+                        class="mb-5"
+                        color="grey lighten-3"
+                        height="500px"
+                        style="padding: 30px;"
+                >
+                    <form>
+                        <v-select
+                                :items="allTags"
+                                v-model="tags"
+                                chips
+                                multiple
+                                label="Tags"
+                                style="margin: 3%;"
+                        ></v-select>
+                    </form>
+                </v-card>
+
+                <v-btn
+                        color="primary"
+                        @click="stage = 3"
+                >
+                    Weiter
+                </v-btn>
+
+                <v-btn flat @click="stage = 1">Zurück</v-btn>
+            </v-stepper-content>
+
+            <v-stepper-content step="3">
+                <v-card
+                        class="mb-5"
+                        color="grey lighten-3"
+                        height="500px"
+                        style="padding: 30px;"
+                >
+                    <form>
+                        <v-layout justify-space-between row style="margin: 3%;">
+                            Logo:
+                            <input type="file" @change="onFileSelected">
+                            <v-img :src="getScreenshotUrl(logo)" v-if="logo!=null" contain height="50px"></v-img>
+                        </v-layout>
+                        <v-layout justify-space-between row style="margin: 3%;">
+                            Screenshots:
+                            <input multiple type="file" @change="onScreenshotFilesSelected">
+                            <v-layout row v-for="pic in screenshotFiles">
+                                <v-img :src="getScreenshotUrl(pic)" v-if="pic!=null" contain height="50px"></v-img>
+                            </v-layout>
+                        </v-layout>
+                    </form>
+                </v-card>
+
+                <v-btn
+                        color="primary"
+                        @click="uploadFiles()"
+                >
+                    Los gehts!
+                </v-btn>
+            </v-stepper-content>
+        </v-stepper-items>
+    </v-stepper>
+
 </template>
 
 <script>
@@ -51,6 +117,8 @@
     export default {
         name: "CreateApp",
         data: () => ({
+            stage: 1,
+            appId: null,
             title: '',
             description: '',
             screenshotFiles: [],
@@ -83,7 +151,7 @@
                 console.log(screenID)
                 return URL.createObjectURL(screenID)
             },
-            createApp: function (event) {
+            createApp: function () {
                 axios.post(this.getCreateUrl(),
                     {
                         'description': this.description,
@@ -97,43 +165,47 @@
                         },
                     }
                 ).then(res => {
-                        let idofAPP = res.data.id
-                        //Save Logo
-                        if (this.logo != null) {
-                            const fd = new FormData();
-                            fd.append('file', this.logo)
-                            axios.post("/api/users/" + this.getUser().id + "/apps/" + idofAPP + "/logo",
-                                fd,
-                                {
-                                    headers: {
-                                        'Content-Type': 'multipart/form-data',
-                                        'Authorization': 'Bearer ' + this.getUser().token
-                                    },
-                                }
-                            )
-                                .then(() => router.push({name: 'home'}))
-                                .catch(() => Promise.reject('Fehler beim Hochladen des des Logos!'))
+                    this.appId = res.data.id;
+                    this.stage = 2;
+                })
+                    .catch(() => Promise.reject('Die App konnte nicht erstellt werden!'))
+
+
+            },
+            uploadFiles: function () {
+                //Save Logo
+                if (this.logo != null) {
+                    const fd = new FormData();
+                    fd.append('file', this.logo);
+                    axios.post("/api/users/" + this.getUser().id + "/apps/" + this.appId + "/logo",
+                        fd,
+                        {
+                            headers: {
+                                'Content-Type': 'multipart/form-data',
+                                'Authorization': 'Bearer ' + this.getUser().token
+                            },
                         }
+                    )
+                        .then(() => router.push({name: 'home'}))
+                        .catch(() => Promise.reject('Fehler beim Hochladen des des Logos!'))
+                }
 
 
-                        //Save screenshots
-                        for (let i = 0; i < this.screenshotFiles.length; i++) {
-                            const screenData = new FormData();
-                            console.log('upload Screenshot: ' + this.screenshotFiles[i].name)
-                            screenData.append('file', this.screenshotFiles[i])
+                //Save screenshots
+                for (let i = 0; i < this.screenshotFiles.length; i++) {
+                    const screenData = new FormData();
+                    console.log('upload Screenshot: ' + this.screenshotFiles[i].name);
+                    screenData.append('file', this.screenshotFiles[i]);
 
-                            axios.post("/api/users/" + this.getUser().id + "/apps/" + idofAPP + "/screenshots", screenData,
-                                {
-                                    headers: {
-                                        'Authorization': 'Bearer ' + this.getUser().token
-                                    },
-                                }
-                            )
-                                .catch(() => Promise.reject('Fehler beim Hochladen des Screenshots!'))
+                    axios.post("/api/users/" + this.getUser().id + "/apps/" + this.appId + "/screenshots", screenData,
+                        {
+                            headers: {
+                                'Authorization': 'Bearer ' + this.getUser().token
+                            },
                         }
-                    }
-                ).catch(err => console.error(err)
-                )
+                    )
+                        .catch(() => Promise.reject('Fehler beim Hochladen des Screenshots!'))
+                }
 
 
             }
